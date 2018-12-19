b26fa421593f597c6a3ce15e54b0c60f38ff6796
Matheus Nerone <matheus@cwi.nl>
2018-12-19 11:49:24 +0100

    fixing bugs and improving performance

--- dynamic_bitset.cpp
+++ dynamic_bitset.cpp
@@ -4,30 +4,33 @@
 #include "string.h"
 #include <math.h>
 
-DynamicBitSet::DynamicBitSet(uint64_t n_of_bits) {
-    bitset_size = ceil(n_of_bits / (double)min_element_size_bits());
-    n_of_bits = n_of_bits;
+DynamicBitSet::DynamicBitSet(uint64_t number_of_bits) {
+    bitset_size = ceil(number_of_bits / (double)min_element_size_bits);
+    n_of_bits = number_of_bits;
     bitset = new min_element_size[bitset_size];
-    memset(bitset, 0, bitset_size * min_element_size_bits()/8);
+    memset(bitset, 0, bitset_size * min_element_size_bits/8);
 }
 
 void DynamicBitSet::setBitToOne(uint64_t bit) {
-    bitset[bit / min_element_size_bits()] |= (1 << (bit % min_element_size_bits()));
+    // bitset[bit / min_element_size_bits] |= (one << (bit % min_element_size_bits));
+    bitset[bit >> log_of_min_element] |= (one << (bit & (min_element_size_bits - 1)));
 }
 
 void DynamicBitSet::setBitToZero(uint64_t bit) {
-    bitset[bit / min_element_size_bits()] &= ~(1 << (bit % min_element_size_bits()));
+    // bitset[bit / min_element_size_bits] &= ~(one << (bit % min_element_size_bits));
+    bitset[bit >> log_of_min_element] &= ~(one << (bit & (min_element_size_bits - 1)));
 }
 
 bool DynamicBitSet::readBit(uint64_t bit) {
-    min_element_size value = bitset[bit / min_element_size_bits()];
-    return (min_element_size) 1 & (value >> (bit % min_element_size_bits()));
+    min_element_size value = bitset[bit / min_element_size_bits];
+    // return one & (value >> (bit % min_element_size_bits));
+    return one & (value >> (bit & (min_element_size_bits - 1)));
 }
 
 DynamicBitSet DynamicBitSet::intersection(DynamicBitSet b2){
     DynamicBitSet result = DynamicBitSet(n_of_bits);
 
-    for(size_t i = 0; i < bitset_size; ++i){
+    for(uint64_t i = 0; i < bitset_size; ++i){
         result.bitset[i] = this->bitset[i] & b2.bitset[i];
     }
 
@@ -35,23 +38,19 @@ DynamicBitSet DynamicBitSet::intersection(DynamicBitSet b2){
 }
 
 void DynamicBitSet::intersection_on_self(DynamicBitSet b2){
-    for(size_t i = 0; i < bitset_size; ++i){
+    for(uint64_t i = 0; i < bitset_size; ++i){
         this->bitset[i] = this->bitset[i] & b2.bitset[i];
     }
 }
 
 void DynamicBitSet::print(){
 
-    for(size_t i = 0; i < bitset_size * min_element_size_bits(); i++)
+    for(uint64_t i = 0; i < bitset_size * min_element_size_bits; i++)
     {
-        if(i % min_element_size_bits() == 0)
+        if(i % min_element_size_bits == 0)
             printf("|");
         printf("%d", readBit(i));
     }
     printf("\n");
 
-}
-
-inline uint64_t DynamicBitSet::min_element_size_bits(){
-    return sizeof(min_element_size)*8;
 }
--- dynamic_bitset.h
+++ dynamic_bitset.h
@@ -9,14 +9,16 @@ private:
 
     typedef uint64_t min_element_size;
 
+    const min_element_size one = 1;
+    const min_element_size min_element_size_bits = sizeof(min_element_size) * 8;
+    const min_element_size log_of_min_element = log2(min_element_size_bits);
+
     min_element_size *bitset;
     uint64_t bitset_size;
     uint64_t n_of_bits;
 
-    uint64_t min_element_size_bits();
-
 public:
-    DynamicBitSet(uint64_t n_of_bits);
+    DynamicBitSet(uint64_t number_of_bits);
 
     void setBitToOne(uint64_t bit);
     void setBitToZero(uint64_t bit);
